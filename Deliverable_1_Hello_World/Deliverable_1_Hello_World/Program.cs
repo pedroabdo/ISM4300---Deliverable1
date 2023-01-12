3dd5caf46dc3767abe6caa32cfd344098c92f604
pedroabdo <122575177+pedroabdo@users.noreply.github.com>
2023-01-12 17:42:44 -0500

    Add files via upload

---
+++ Deliverable_1_Hello_World/Deliverable_1_Hello_World/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace deliverable_helloworld
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("Type out your user name ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The user name is: " + userName);
+
+            Console.WriteLine("Type out your user's age ");
+            int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + userAge);
+        }
+    }
+}
